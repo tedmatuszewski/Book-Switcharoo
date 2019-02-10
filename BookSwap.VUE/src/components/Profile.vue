509977fbbb8a800c96465303b12f953d178b0e64
Ted Matuszewski <ted.matuszewski@outlook.com>
2019-02-10 18:16:16 -0500

    Have a mock up of the messaging section of the site.

--- BookSwap.VUE/src/components/Profile.vue
+++ BookSwap.VUE/src/components/Profile.vue
@@ -1,28 +1,194 @@
 <template>
     <div class="container-fluid">
-        <div class="row mt-4">
-            <div class="col-sm-8">
-                <h3>Your library</h3>
+        <div class="card mt-3">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                    <li class="nav-item"><a v-bind:class="{ active: ActiveTab == 'library' }" v-on:click.prevent="tabChange('library')" class="nav-link" data-toggle="tab" href="#library" role="tab">Library</a> </li>
+                    <li class="nav-item"><a v-bind:class="{ active: ActiveTab == 'message' }" v-on:click.prevent="tabChange('message')" class="nav-link" data-toggle="tab" href="#messages" role="tab">Messages</a> </li>
+                </ul>
+            </div>
 
-                <div class="card mb-3" v-for="(book, index) in Books" :key="book.Id">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-4 col-md-3 col-lg-2">
-                                <img :src="book.Image" :alt="book.Title" class="img-fluid" style="max-height:150px;">
+            <div class="card-body tab-content">
+                <div class="tab-pane fade" v-bind:class="{ 'active': ActiveTab == 'library', 'show': ActiveTab == 'library' }" id="library" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <div class="row mb-4 pb-3 border-bottom" v-for="(book, index) in Books" :key="book.Id">
+                        <div class="col-sm-4 col-md-3 col-lg-2">
+                            <img :src="book.Image" :alt="book.Title" class="img-fluid" style="max-height:150px;">
+                        </div>
+                        <div class="col-sm-8">
+                            <h5 class="card-title">{{book.Title}}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{book.Isbn}}</h6>
+                            <p class="card-text">{{book.Description}}.</p>
+                            <router-link class="card-link" tag="a" :to="{ name: 'editpost', params: { id: book.Id }}">Edit</router-link>
+                            <a href="#" v-on:click.prevent="deleteBook(book, index)" class="card-link">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" v-bind:class="{ 'active': ActiveTab == 'message', 'show': ActiveTab == 'message' }"  id="messages" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    <div class="messaging">
+                        <div class="inbox_msg">
+                            <div class="inbox_people">
+                                <div class="headind_srch">
+                                    <div class="recent_heading">
+                                        <h4>Recent</h4>
+                                    </div>
+                                    <div class="srch_bar">
+                                        <div class="stylish-input-group">
+                                            <input type="text" class="search-bar" placeholder="Search">
+                                            <span class="input-group-addon">
+                                                <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="inbox_chat">
+                                    <div class="chat_list active_chat">
+                                        <div class="chat_people">
+                                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                            <div class="chat_ib">
+                                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                                <p>
+                                                    Test, which is a new approach to have all solutions
+                                                    astrology under one roof.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="chat_list">
+                                        <div class="chat_people">
+                                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                            <div class="chat_ib">
+                                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                                <p>
+                                                    Test, which is a new approach to have all solutions
+                                                    astrology under one roof.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="chat_list">
+                                        <div class="chat_people">
+                                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                            <div class="chat_ib">
+                                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                                <p>
+                                                    Test, which is a new approach to have all solutions
+                                                    astrology under one roof.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="chat_list">
+                                        <div class="chat_people">
+                                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                            <div class="chat_ib">
+                                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                                <p>
+                                                    Test, which is a new approach to have all solutions
+                                                    astrology under one roof.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="chat_list">
+                                        <div class="chat_people">
+                                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                            <div class="chat_ib">
+                                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                                <p>
+                                                    Test, which is a new approach to have all solutions
+                                                    astrology under one roof.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="chat_list">
+                                        <div class="chat_people">
+                                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                            <div class="chat_ib">
+                                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                                <p>
+                                                    Test, which is a new approach to have all solutions
+                                                    astrology under one roof.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="chat_list">
+                                        <div class="chat_people">
+                                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                            <div class="chat_ib">
+                                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                                <p>
+                                                    Test, which is a new approach to have all solutions
+                                                    astrology under one roof.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-8">
-                                <h5 class="card-title">{{book.Title}}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">{{book.Isbn}}</h6>
-                                <p class="card-text">{{book.Description}}.</p>
-                                <router-link class="card-link" tag="a" :to="{ name: 'editpost', params: { id: book.Id }}">Edit</router-link>
-                                <a href="#" v-on:click.prevent="deleteBook(book, index)" class="card-link">Delete</a>
+                            <div class="mesgs">
+                                <div class="msg_history">
+                                    <div class="incoming_msg">
+                                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                        <div class="received_msg">
+                                            <div class="received_withd_msg">
+                                                <p>
+                                                    Test which is a new approach to have all
+                                                    solutions
+                                                </p>
+                                                <span class="time_date"> 11:01 AM    |    June 9</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="outgoing_msg">
+                                        <div class="sent_msg">
+                                            <p>
+                                                Test which is a new approach to have all
+                                                solutions
+                                            </p>
+                                            <span class="time_date"> 11:01 AM    |    June 9</span>
+                                        </div>
+                                    </div>
+                                    <div class="incoming_msg">
+                                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                        <div class="received_msg">
+                                            <div class="received_withd_msg">
+                                                <p>Test, which is a new approach to have</p>
+                                                <span class="time_date"> 11:01 AM    |    Yesterday</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="outgoing_msg">
+                                        <div class="sent_msg">
+                                            <p>Apollo University, Delhi, India Test</p>
+                                            <span class="time_date"> 11:01 AM    |    Today</span>
+                                        </div>
+                                    </div>
+                                    <div class="incoming_msg">
+                                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                        <div class="received_msg">
+                                            <div class="received_withd_msg">
+                                                <p>
+                                                    We work directly with our designers and suppliers,
+                                                    and sell direct to you, which means quality, exclusive
+                                                    products, at a price anyone can afford.
+                                                </p>
+                                                <span class="time_date"> 11:01 AM    |    Today</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="type_msg">
+                                    <div class="input_msg_write">
+                                        <input type="text" class="write_msg" placeholder="Type a message" />
+                                        <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-4">
-
             </div>
         </div>
     </div>
@@ -32,7 +198,8 @@
     export default {
         data() {
             return {
-                Books: []
+                Books: [],
+                ActiveTab: "library"
             }
         },
         methods: {
@@ -42,6 +209,9 @@
                 self.$http.delete("/v1/books/" + book.Id).then(() => {
                     self.Books.splice(index, 1);
                 });
+            },
+            tabChange(tab) {
+                this.ActiveTab = tab;
             }
         },
         mounted() {
