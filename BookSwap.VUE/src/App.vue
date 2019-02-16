362513d75c5e955b215aae2fd4ba2ca5d194f4fc
Ted Matuszewski <ted.matuszewski@outlook.com>
2019-02-15 21:11:38 -0500

    Fixed the footer from not sticking to the bottom of the page.

--- BookSwap.VUE/src/App.vue
+++ BookSwap.VUE/src/App.vue
@@ -1,37 +1,42 @@
 <template>
-    <div>
-        <b-navbar toggleable="sm" type="dark" variant="info">
-            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-            <b-navbar-brand href="/">
-                <img src="../public/logo_white.png" style="max-height:40px" />
-                Book Swap
-            </b-navbar-brand>
-            <b-collapse is-nav id="nav_collapse">
-                <b-navbar-nav>
-                    <router-link class="nav-item" tag="li" to="/"><a class="nav-link" href="#">Home</a></router-link>
-                    <router-link class="nav-item" tag="li" to="/post"><a class="nav-link" href="#">Post</a></router-link>
-                </b-navbar-nav>
+    <div class="d-flex flex-column h-100">
+        <header>
+            <nav class="navbar navbar-expand-md navbar-dark bg-info">
+                <a class="navbar-brand" href="#">
+                    <img src="../public/logo_white.png" style="max-height:29px" />
+                    Book Swap
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto">
+                        <router-link class="nav-item" tag="li" to="/"><a class="nav-link" href="#">Home</a></router-link>
+                        <router-link class="nav-item" tag="li" to="/post"><a class="nav-link" href="#">Post</a></router-link>
+                    </ul>
+                    <ul class="navbar-nav ml-auto" v-if="isLoggedIn()">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">{{getUserDisplay()}}</a>
+                            <div class="dropdown-menu" style="margin-top:0; right:0;width: 200px;">
+                                <router-link class="dropdown-item" tag="a" to="/profile">Profile</router-link>
+                                <a class="dropdown-item" href="#" v-on:click.prevent="logout">Signout</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto" v-else>
+                        <router-link class="nav-item" tag="li" to="/login"><a class="nav-link" href="#">Login</a></router-link>
+                    </ul>
+                </div>
+            </nav>
+        </header>
 
-                <b-navbar-nav class="ml-auto" v-if="isLoggedIn()">
-                    <b-nav-item-dropdown right>
-                        <template slot="button-content">
-                            {{getUserDisplay()}}
-                        </template>
-                        <b-dropdown-item to="/profile">Profile</b-dropdown-item>
-                        <b-dropdown-item v-on:click.prevent="logout" href="#">Signout</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                </b-navbar-nav>
-                <b-navbar-nav class="ml-auto" v-else>
-                    <router-link class="nav-item" tag="li" to="/login"><a class="nav-link" href="#">Login</a></router-link>
-                </b-navbar-nav>
-                </b-collapse>
-        </b-navbar>
-
-<router-view />
+        <main role="main" class="flex-shrink-0">
+            <router-view />
+        </main>
 
         <footer class="footer mt-auto py-3">
             <div class="container">
-                <span class="text-muted">Book Swap &copy; 2019</span>
+                <span class="text-muted">Place sticky footer content here.</span>
             </div>
         </footer>
     </div>
--- BookSwap.VUE/src/components/Login.vue
+++ BookSwap.VUE/src/components/Login.vue
@@ -63,4 +63,9 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .form-control:focus {
+        -webkit-box-shadow: none;
+        box-shadow: none;
+        border: 1px solid #ced4da;
+    }
 </style>
