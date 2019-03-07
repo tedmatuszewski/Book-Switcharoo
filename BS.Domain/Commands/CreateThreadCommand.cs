9b0fc3594a194a765e567f7bf9bb8b4b0beef59f
Ted Matuszewski <ted.matuszewski@outlook.com>
2019-03-07 06:36:47 -0500

    Added commands and queries for threads.

---
+++ BS.Domain/Commands/CreateThreadCommand.cs
@@ -0,0 +1,36 @@
+﻿using BS.Domain.Convertors;
+using BS.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BS.Domain.Commands
+{
+    public class CreateThreadCommand : Types.Command<ThreadDTO>
+    {
+        private string _from;
+        private int _bookId;
+
+        public CreateThreadCommand(string from, int bookId)
+        {
+            this._from = from;
+            this._bookId = bookId;
+        }
+
+        public override string Title => "";
+
+        public override void Execute(IUnitOfWork _unit)
+        {
+            var book = _unit.bookRepository.Get(_bookId);
+            var thread = new ThreadDTO(book.User, _from, _bookId);
+            var message = new MessageDTO(thread.Id);
+
+            _unit.threadRepository.Create(ThreadConvertor.Convert(thread));
+            _unit.messageRepository.Create(MessageConvertor.Convert(message));
+            _unit.Save();
+
+            this.Message = "Successfully created thread";
+            this.Result = thread;
+        }
+    }
+}
---
+++ BS.Domain/Queries/GetThreadsByEmailQuery.cs
@@ -0,0 +1,32 @@
+﻿using BS.Domain.Convertors;
+using BS.Domain.Types;
+using BS.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BS.Domain.Queries
+{
+    public class GetThreadsByEmailQuery : Query<IEnumerable<ThreadDTO>>
+    {
+        private string _email;
+
+        public override string Title => "";
+
+        public GetThreadsByEmailQuery(string email)
+        {
+            this._email = email;
+        }
+
+        public override void Execute(IUnitOfWork _unit)
+        {
+            var threads = _unit.threadRepository.Get(_email)
+                            .Select(t => ThreadConvertor.Convert(t).SetMessageTitle(_email))
+                            .ToList();
+
+            this.Result = threads;
+            this.Message = "Successfully retrieved threads";
+        }
+    }
+}
